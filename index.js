eefbba207a408dc665ce6dc2e1a38f1e681cb3ca
BenSegal855 <bensegal@udel.edu>
2020-12-09 18:02:22 -0500

    more peoples

--- index.js
+++ index.js
@@ -7,20 +7,46 @@ module.exports = class SleeperNoSleeping extends Plugin {
     result: 'Something has gone wrong...'
   }
 
-  sleep = async () => {
-    let awake;
-
+  sleep = async (args) => {
     // HACK: Kate is in Customa and Customa has widgets on.
     let { members } = await get('https://discord.com/api/v8/guilds/476844593896226816/widget.json').then(r => r.body);
-
     if (!members) return this.base;
+    
+    let awake = false;
+    let name = "Kate"
+    let person =  members.filter(member => member.avatar_url == "https://cdn.discordapp.com/widget-avatars/2uCBLIlJEJij46l1OQiMNSgtWFQDlXLeaOYtgjhS2cw/NzDGQponSt7zB5zzzKzM8zMBcYUIL1XpD55kw2pV4oyJG7fBVB8pmoiVMfPjgnn9vGr9ribt4ZCIc6D0nO_TtI0EJzfTo-StbRk3iaj6Xl1lMesR9F93qOUoIcy5c0vpJBlK-CCbv8iw8w")[0];
 
     // HACK: This is a bad way to do this as if she changes avatars this will not work. I gotta fix this sometime.
-    if (members.filter(member => member.avatar_url == "https://cdn.discordapp.com/widget-avatars/2uCBLIlJEJij46l1OQiMNSgtWFQDlXLeaOYtgjhS2cw/NzDGQponSt7zB5zzzKzM8zMBcYUIL1XpD55kw2pV4oyJG7fBVB8pmoiVMfPjgnn9vGr9ribt4ZCIc6D0nO_TtI0EJzfTo-StbRk3iaj6Xl1lMesR9F93qOUoIcy5c0vpJBlK-CCbv8iw8w").length >= 1) awake = true;
+    if(args[0]){
+      switch(args[0].toLowerCase()) {
+      case "emma":
+        name = "emma 🌺"
+        person = members.filter(member => member.username === "emma 🌺")[0];
+        break;
+      
+      case "bowser":
+        name = "Bowser"
+        person = members.filter(member => member.avatar_url === "https://cdn.discordapp.com/widget-avatars/E37679PGTHUwSWNtH13rUI2qj0aw11Pp-MNvm3NQ46Y/Kis9InZABkbpZ8l5LS6apzh-i-ofBbLynoA-L8x0CLGqngX_qi9VQXTlvjwHpFk4MstDw5SggO0R1q7mR52zgbw4d7CLwU_XFuJfK0ipFKbDIX_QNaj8pGLsvKmejpegeBnBzEc-KGTp")[0];
+        break;
+      
+
+      case "aeth":
+        name = "Aeth"
+        person = members.filter(member => member.username === "aetheryx")[0];
+        break;
+
+      case "ben":
+        name = "Ben"
+        person = members.filter(member => member.username === "Ben")[0]; // I will change my avatart but not my nick
+        break;
+     }
+    }
+    
+    awake = person?.status === "online";
 
     return {
       ...this.base,
-      result: `Kate is ${awake ? "awake" : "asleep"}.`
+      result: `${name} is ${awake ? "awake" : "asleep"}.`
     };
   }
 
@@ -29,7 +55,27 @@ module.exports = class SleeperNoSleeping extends Plugin {
       command: 'sleepstatus',
       description: 'Is Kate Sleeping?',
       usage: '{c}',
-      executor: this.sleep
+      executor: (args) => this.sleep(args),
+      autocomplete: (args) => {
+        if (args[1] === void 0) {
+          return {
+            header: "Whos sleeping?",
+            commands: [
+              {
+                command: "Kate"
+              }, {
+                command: "emma"
+              }, {
+                command: "Aeth"
+              }, {
+                command: "Bowser"
+              }, {
+                command: "Ben"
+              }
+            ].filter(({ command }) => command.toLowerCase().includes(args[0]?.toLowerCase()))
+          };
+        }
+      }
     });
   }
 
