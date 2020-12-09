f7241e78f452a0fe6da77f656f210b9d900ef83e
TheOddGarlic <umutinanerdogan62@gmail.com>
2020-12-10 00:39:02 +0300

    improve

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ module.exports = class SleeperNoSleeping extends Plugin {
 
     if (!members) return this.base;
 
-    // HACK: This is a bad way to do this as if someone nicks as her or she changes nicks this will not work. I gotta fix this sometime.
-    if (members.filter(member => member.username == "katlyn").length >= 1) awake = true;
+    // HACK: This is a bad way to do this as if she changes avatars this will not work. I gotta fix this sometime.
+    if (members.filter(member => member.avatar_url == "https://cdn.discordapp.com/widget-avatars/2uCBLIlJEJij46l1OQiMNSgtWFQDlXLeaOYtgjhS2cw/NzDGQponSt7zB5zzzKzM8zMBcYUIL1XpD55kw2pV4oyJG7fBVB8pmoiVMfPjgnn9vGr9ribt4ZCIc6D0nO_TtI0EJzfTo-StbRk3iaj6Xl1lMesR9F93qOUoIcy5c0vpJBlK-CCbv8iw8w").length >= 1) awake = true;
 
     return {
       ...this.base,
